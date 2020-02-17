670c306a0e79d107ad83a307498a526901b488ca
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2020-02-17 09:44:57 +0530

    View Encapulation., template and styles covered

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,11 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ProductDetails, ProductService } from '../service/product.service';
 import { CartService } from '../shopping/cart.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  //   template : `
+  //   <h2 class="text-center">Product List</h2>
+  // <div>
+  //     <h4>Added product!!</h4>
+  //     {{ cartService.cartProducts | json }}
+  // </div>
+  // <div class="flex-cotainer-wrap">
+  //     <div class="m-2" *ngFor="let product of products">
+  //         <app-product-view title="product view" [productDetails]="product" (addToCart)="addToCart($event)"></app-product-view>
+  //     </div>
+  // </div>`,
+  styleUrls: ['./product-list.component.scss', './product-lists.component.scss'],
+  styles: ['h4{font-style:italic}'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class ProductListComponent implements OnInit {
   products: ProductDetails[] = [];
