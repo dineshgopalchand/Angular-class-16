8ccfe1f305bdedc979d463e53e6eda0703eccdee
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2020-02-19 09:33:38 +0530

    ngIf and ngFor directive completed

--- src/app/directive/directive.component.ts
+++ src/app/directive/directive.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-directive',
   templateUrl: './directive.component.html',
-  styleUrls: ['./directive.component.scss']
+  styleUrls: ['./directive.component.scss'],
 })
 export class DirectiveComponent implements OnInit {
 
-  courseList: any[] = [];
+  courseList: CourseType[] = [];
   constructor(private courseService: CoursesService) { }
 
   ngOnInit() {
-    // this.courseList = this.courseService.courseList;
+    this.courseList = this.courseService.courseList;
+  }
+  switchCourseStatus() {
+    this.courseList = this.courseList.length ? [] : this.courseService.courseList;
+  }
+  addCourse() {
+    this.courseList.push({
+      id: 34,
+      name: 'Ruby'
+    });
+  }
+  removeCourse(course: CourseType) {
+    // const index = this.courseList.indexOf(course);
+    const index = this.courseList.indexOf(course);
+    this.courseList.splice(index, 1);
   }
 
+}
+
+export interface CourseType {
+  id: number;
+  name: string;
 }
