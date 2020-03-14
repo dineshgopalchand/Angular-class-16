30adc5dfbddc54e041bd57ccaa91b9c621b8182e
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2020-03-14 09:10:39 +0530

    Form Array Completed

--- src/app/forms/to-do-list/to-do-list.component.ts
+++ src/app/forms/to-do-list/to-do-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormControl, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do-list',
@@ -7,13 +8,34 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class ToDoListComponent implements OnInit {
   // @ViewChild('newtopic', { static: false }) newTopic: ElementRef;
+
+  topicField = new FormControl('');
+  topicList = new FormArray([]);
+
   constructor() { }
 
   ngOnInit() {
   }
-  addTopic(inputElement: HTMLInputElement) {
-    console.log(inputElement.value);
+  // addTopic(inputElement: HTMLInputElement) {
+  //   console.log(inputElement.value);
+  //   // console.log((this.newTopic.nativeElement as HTMLInputElement).value);
+  //   inputElement.value = '';
+  // }
+  addTopic() {
+    console.log(this.topicField.value);
+    this.topicList.controls.push(this.topicField.value);
     // console.log((this.newTopic.nativeElement as HTMLInputElement).value);
-    inputElement.value = '';
+    this.topicField.setValue('');
+    console.log(this.topicList);
+  }
+
+  get topicLists() {
+    return this.topicList.controls;
+  }
+  removeTopic(item) {
+    // console.log('remove topic function called');
+    const indexVal = this.topicLists.indexOf(item);
+    this.topicLists.splice(indexVal, 1);
+    console.log(this.topicList);
   }
 }
